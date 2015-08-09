c60a7fe7efd0452dcf772a48d514623c06d455ff
Janis Bebriss <janis@fnet.lv>
2015-08-09 13:36:32 +0300

    brackets fix

--- src/pageable/pager.tpl.js
+++ src/pageable/pager.tpl.js
@@ -5,8 +5,8 @@ angular.module('ngQuantum.pageable.pager')
     var END   = $interpolate.endSymbol();
     $templateCache.put('pageable/pager.tpl.html',
         '<ul class="pager">'
-            + '<li ng-class="{disabled:$currentPage < 2, previous:$aligned}" class="page-button previous"><a role="button" tabindex="0" ng-click="$prev()"><i class="fic fu-angle-l"></i> " + START + "$locale.previousText' + END+ '</a></li>'
-            + '<li ng-class="{disabled:$totalPages == $currentPage, previous:$aligned}" class="page-button next"><a role="button" tabindex="0" ng-click="$next()">" + START + "$locale.nextText' + END+ ' <i class="fic fu-angle-r"></i> </a></li>'
+            + '<li ng-class="{disabled:$currentPage < 2, previous:$aligned}" class="page-button previous"><a role="button" tabindex="0" ng-click="$prev()"><i class="fic fu-angle-l"></i> ' + START + '$locale.previousText' + END+ '</a></li>'
+            + '<li ng-class="{disabled:$totalPages == $currentPage, previous:$aligned}" class="page-button next"><a role="button" tabindex="0" ng-click="$next()">' + START + '$locale.nextText' + END+ ' <i class="fic fu-angle-r"></i> </a></li>'
         + '</ul>'
         + '<div ng-show="$pagerWarning"><span class="label label-warning"></span><span ng-bind="$pagerWarning"></span></div>'
     );
--- src/pageable/pagination.tpl.js
+++ src/pageable/pagination.tpl.js
@@ -5,11 +5,11 @@ angular.module('ngQuantum.pageable.pagination')
     var END   = $interpolate.endSymbol();
     $templateCache.put('pageable/pagination.tpl.html',
         '<ul class="pagination" ng-class="$cssClass" ng-show="$totalPages > 1">'
-            + '<li ng-if="$firstLast" ng-class="{disabled:$currentPage < 2}" class="page-button first"><a role="button" tabindex="0" ng-click="$first()" class="titip-top" data-title="" + START + "$locale.firstText' + END+ '"><i class="fic fu-arrow-left"></i></a></li>'
-            + '<li ng-if="$prevNext" ng-class="{disabled:$currentPage < 2}" class="page-button previous"><a role="button" tabindex="0" ng-click="$prev()" class="titip-top" data-title="" + START + "$locale.previousText' + END+ '"><i class="fic fu-angle-l"></i></a></li>'
-            + '<li class="page-number" ng-class="{active:page == $currentPage, lastActive: $totalPages == $currentPage}" ng-repeat="page in pages"><a role="button" tabindex="0" ng-click="$gotoPage(page)">" + START + "page' + END+ '</a></li>'
-            + '<li ng-if="$prevNext"  ng-class="{disabled:$totalPages == $currentPage}" class="page-button next"><a role="button" tabindex="0" ng-click="$next()"  class="titip-top" data-title="" + START + "$locale.nextText' + END+ '"><i class="fic fu-angle-r"></i></a></li>'
-            + '<li ng-if="$firstLast" ng-class="{disabled:$totalPages == $currentPage}" class="page-button last"><a role="button" tabindex="0" class="titip-top" data-title="" + START + "$locale.lastText' + END+ '" ng-click="$last()"><i class="fic fu-arrow-right"></i></a></li>'
+            + '<li ng-if="$firstLast" ng-class="{disabled:$currentPage < 2}" class="page-button first"><a role="button" tabindex="0" ng-click="$first()" class="titip-top" data-title="' + START + '$locale.firstText' + END+ '"><i class="fic fu-arrow-left"></i></a></li>'
+            + '<li ng-if="$prevNext" ng-class="{disabled:$currentPage < 2}" class="page-button previous"><a role="button" tabindex="0" ng-click="$prev()" class="titip-top" data-title="' + START + '$locale.previousText' + END+ '"><i class="fic fu-angle-l"></i></a></li>'
+            + '<li class="page-number" ng-class="{active:page == $currentPage, lastActive: $totalPages == $currentPage}" ng-repeat="page in pages"><a role="button" tabindex="0" ng-click="$gotoPage(page)">' + START + 'page' + END+ '</a></li>'
+            + '<li ng-if="$prevNext"  ng-class="{disabled:$totalPages == $currentPage}" class="page-button next"><a role="button" tabindex="0" ng-click="$next()"  class="titip-top" data-title="' + START + '$locale.nextText' + END+ '"><i class="fic fu-angle-r"></i></a></li>'
+            + '<li ng-if="$firstLast" ng-class="{disabled:$totalPages == $currentPage}" class="page-button last"><a role="button" tabindex="0" class="titip-top" data-title="' + START + '$locale.lastText' + END+ '" ng-click="$last()"><i class="fic fu-arrow-right"></i></a></li>'
         + '</ul>'
         + '<div ng-show="$pagerWarning"><span class="label label-warning"></span><span ng-bind="$pagerWarning"></span></div>'
     );
